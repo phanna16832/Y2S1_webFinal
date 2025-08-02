443d9235927f90c9fc7ce6126917e10948336f25
phanna123 <soksiphanna35@gmail.com>
2025-08-02 20:40:05 +0700

    update navbar

---
+++ php/index.php
@@ -0,0 +1,15 @@
+<?php
+ 
+echo "<h1>Welcome to My Website</h1>";
+?>
+
+<body>
+    
+<nav>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Contact U</a></li>
+    </ul>
+</nav>
+</body>
