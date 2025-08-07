f826edc4f1c8b8c69d4aef0d93dfd6743c3f9ea7
phanna123 <soksiphanna35@gmail.com>
2025-08-07 17:51:39 +0700

    update header + container :technologist:

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var age = 3;
+var agePow = age ** 2;
+console.log(`Age: ${age}, Age Squared: ${agePow}`);
+//# sourceMappingURL=index.js.map
