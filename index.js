6538c86c719c8dc54aa9ffddab3a827bd37feca9
phanna123 <soksiphanna35@gmail.com>
2025-08-11 14:06:00 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var age = 3;
-var agePow = age ** 2;
-console.log(`Age: ${age}, Age Squared: ${agePow}`);
-//# sourceMappingURL=index.js.map
+const dateComponent = document.querySelector(".pagination");
+const today = new Date();
+
+let i = 0;
+while (i <= 7) {
+  let future = new Date(today);
+  future.setDate(today.getDate() + i);
+
+  const span = document.createElement("span");
+  span.innerText = future.toLocaleDateString("en-US", { weekday: "long" });
+
+  const h1 = document.createElement("h1");
+  h1.innerText = future.getDate();
+
+const newDiv = document.createElement("div");
+newDiv.className = "box";
+
+  dateComponent.appendChild(newDiv);
+  newDiv.appendChild(span);
+  newDiv.appendChild(h1);
+newDiv.style.background = "gray";
+span.style.color = "#fff";
+span.style.fontSize = "clamp(14px, 16px)";
+h1.style.color = "#fff";
+h1.style.fontSize =  "clamp(14px, 16px)";
+    i++;
+}
---
+++ test.js
@@ -0,0 +1,58 @@
+// // script.js
+
+// // Translations
+// const translations = {
+//   en: {
+//     welcome: "Welcome to our website!",
+//     description: "This is an example of a multi-language site.",
+//     contact: "Contact Us"
+//   },
+//   zh: {
+//     welcome: "欢迎来到我们的网站！",
+//     description: "这是一个多语言网站的示例。",
+//     contact: "联系我们"
+//   },
+//   km: {
+//     welcome: "សូមស្វាគមន៍មកកាន់គេហទំព័ររបស់យើង!",
+//     description: "នេះគឺជាគំរូនៃគេហទំព័រពហុភាសា។",
+//     contact: "ទំនាក់ទំនងយើង"
+//   }
+// };
+
+// // Function to update all text
+// function changeLanguage(lang) {
+//   document.querySelectorAll("[data-translate]").forEach(el => {
+//     const key = el.getAttribute("data-translate");
+//     el.innerText = translations[lang][key];
+//   });
+
+//   // Save language to localStorage
+//   localStorage.setItem("selectedLanguage", lang);
+// }
+
+// // On language change
+// document.getElementById("languageSwitcher").addEventListener("change", function() {
+//   changeLanguage(this.value);
+// });
+
+// // On page load — use saved language or default
+// window.addEventListener("DOMContentLoaded", () => {
+//   const savedLang = localStorage.getItem("selectedLanguage") || "en";
+//   document.getElementById("languageSwitcher").value = savedLang;
+//   changeLanguage(savedLang);
+// });
+
+
+document.querySelectorAll("a[data-link]").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault(); // Stop full page reload
+    const url = link.getAttribute("href");
+
+    fetch(url)
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById("content").innerHTML = html;
+      })
+      .catch(err => console.error("Error loading page:", err));
+  });
+});
